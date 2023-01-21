486b705cbbe5e032fd54c316317fc8f042767d80
unknown <Your Name>
2023-01-20 20:15:16 -0500

    Adding for loop, steps, and time delay in the physics engine.

--- simulate.py
+++ simulate.py
@@ -1,3 +1,10 @@
 import pybullet as p
+import time
 physicsClient = p.connect(p.GUI)
+#list for use in for loop
+list = range(1000)
+for n in list:
+    p.stepSimulation()
+    time.sleep(1/60)
+    print(n)
 p.disconnect()
