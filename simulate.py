6a6eeffe7a5d6430c6c752fea2212a970a75ad77
Max Bradford <mjbradfo@uvm.edu>
2023-01-27 00:49:55 -0500

    "Testing commit and updating simulate.py"

--- simulate.py
+++ simulate.py
@@ -1,8 +1,13 @@
 import pybullet as p
+import pybullet_data
 import time
 physicsClient = p.connect(p.GUI)
+p.setADditionalSearchPath(pybullet_data.getDataPath())
 #list for use in for loop
 list = range(1000)
+p.setGravity(0,0,-9.8)
+planeId = p.loadURDF("plane.urdf")
+p.loadSDF("box.sdf")
 for n in list:
     p.stepSimulation()
     time.sleep(1/60)
